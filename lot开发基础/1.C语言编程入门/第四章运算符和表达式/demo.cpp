df9ac32447f4edadb2055a2585d3ac05b9e11e53
Wxw <weiwei_2024024@qq.com>
2025-07-06 11:08:14 +0800

    modify

---
+++ lot开发基础/1.C语言编程入门/第四章运算符和表达式/demo.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main() {
+    char x = 120;
+    printf("%d\n", x << 2);
+
+    int a = 3;
+    int b;
+    b = a * 2, a + 4;
+    printf("%d\n", b);
+
+    printf("%d\n", 3 > 4 ? 3 : 4);
+}
